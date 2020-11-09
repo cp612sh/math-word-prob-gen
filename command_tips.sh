1ed10e2ee9ab349e893eb64f6cd892cf97c4e9cc
cp612sh <cp612sh@gmail.com>
2020-11-09 22:58:52 +0800

    ignore pyc

---
+++ command_tips.sh
@@ -0,0 +1,2 @@
+echo '# Blank filling'
+scp shangling@140.109.19.51:~/ntg2/{bertio/bert_bt_nn_300-mathqa-300-45-3-far-1000-finetuned_bert-3.in,bertio/bert_bt_nn_300-mathqa-300-45-3-far-1000-finetuned_bert-3-1.out} ~/mwp/ntg2/bertio
